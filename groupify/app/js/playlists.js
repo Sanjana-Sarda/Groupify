e36d777323406aca600de17a5b792b546705712e
taher1499 <89098047+taher1499@users.noreply.github.com>
2022-02-13 13:30:52 -0800

    Create playlists.js

---
+++ groupify/app/js/playlists.js
@@ -0,0 +1,13 @@
+class Playlists extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+    
+  <iframe src="https://open.spotify.com/embed/playlist/0FYV80FmDwFh4rvpknPhR0" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+ReactDOM.render(
+    <Playlist />,
+    document.getElementById('display-playlist')
+);
+
