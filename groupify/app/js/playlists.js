504531f58aee0e62a076921ddd3c55de1a886715
taher1499 <89098047+taher1499@users.noreply.github.com>
2022-03-08 20:12:37 -0800

    newfrontend

--- groupify/app/js/playlists.js
+++ groupify/app/js/playlists.js
@@ -1,4 +1,5 @@
 class Playlists extends React.Component {
+<<<<<<< Updated upstream
     constructor() {
         super();
         this.state = {};
@@ -8,6 +9,48 @@ class Playlists extends React.Component {
             <iframe src={`https://open.spotify.com/embed/playlist/`+getCookie('playlist_id')} width="600" height="800" align="middle" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
         )
     }
+=======
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div class="playlist-display">
+          <h2 style={{ fontSize: "20px", align: "center" }}>
+            Enjoy your group playlist!
+          </h2>
+        </div>
+        <br />
+        <iframe
+          src={
+            `https://open.spotify.com/embed/playlist/` +
+            getCookie("playlist_id")
+          }
+          width="600"
+          height="400"
+          align="middle"
+          frameBorder="0"
+          allowtransparency="true"
+          allow="encrypted-media"
+        />
+      </div>
+    );
+  }
+>>>>>>> Stashed changes
 }
     
 
